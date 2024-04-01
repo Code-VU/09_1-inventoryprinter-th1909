52856cff7022a82f789ceec36f4675315e32d078
unknown <you@example.com>
2024-04-01 18:19:04 -0500

    9.1 complete

--- displayInventory.py
+++ displayInventory.py
@@ -1,7 +1,13 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
+ 
 def displayInventory(inventory):
-    # your code goes here
+    count = 0
+    for k, v in stuff.items(): 
+        print(v, k)
+    for value in stuff.values():
+        count += value
+    print(f'Total number of items: {count}')
 
 
 if __name__ == "__main__":
